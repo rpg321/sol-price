c7cafdd326df655effe208da4e0a8ddda9bd241c
ross93ft <36698164+ross93ft@users.noreply.github.com>
2023-06-20 17:31:02 +0100

    comment

--- app/api/getPrice.js
+++ app/api/getPrice.js
@@ -1,10 +1,8 @@
 export default async function getPrice() {
-	//url = "blog-1";
-
 	const res = await fetch(
 		"https://api.coingecko.com/api/v3/simple/price?ids=solana&vs_currencies=gbp",
 		{
-			next: { revalidate: 60 }, //60 seconds
+			next: { revalidate: 60 }, //refresh cache after 60 seconds
 		}
 	);
 
